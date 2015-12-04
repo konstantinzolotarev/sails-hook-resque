ce4778cdf7c1b740a40808555971f3583e65ebaa
Konstantin Zolotarev <konstantin.zolotarev@gmail.com>
2015-12-03 23:02:21 -0500

    enqueueIn test

---
+++ test/queue/enqueueIn.test.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var expect = require('chai').expect;
+var _  = require('lodash');
+var sinon = require('sinon');
+
+/* jshint maxlen: false */
+describe('Queue - enqueueIn :: ', function () {
+
+  it('resque.queue should exist', function () {
+    expect(sails.resque.queue).to.be.an('object');
+  });
+
+  it('should enqueue a task', function (done) {
+    sails.resque.queue.enqueueIn(500, 'math', 'add', [1, 2], function (err, enqueued) {
+      expect(err).to.not.exist;
+      expect(enqueued).to.be.true;
+
+      sails.resque.queue.length('math', function (err, value) {
+        expect(err).to.not.exist;
+        expect(value).to.be.eq(1);
+        done();
+      });
+    });
+  });
+
+});
